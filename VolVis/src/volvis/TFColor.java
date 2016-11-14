06fd187e99019b255b04bae13821895ced710126
Ruud Andriessen <r.andriessen@ilysian.com>
2016-11-14 19:28:33 +0100

    Added to aRGB method

--- VolVis/src/volvis/TFColor.java
+++ VolVis/src/volvis/TFColor.java
@@ -21,7 +21,16 @@ public class TFColor {
         b = blue;
         a = alpha;
     }
-    
+
+    public int toARGB() {
+        int c_alpha = a <= 1.0 ? (int) Math.floor(a * 255) : 255;
+        int c_red = r <= 1.0 ? (int) Math.floor(r * 255) : 255;
+        int c_green = g <= 1.0 ? (int) Math.floor(g * 255) : 255;
+        int c_blue = b <= 1.0 ? (int) Math.floor(b * 255) : 255;
+        int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+        return pixelColor;
+    }
+
     @Override
     public String toString() {
         String text = "(" + r + ", " + g + ", " + b + ", " + a + ")";
